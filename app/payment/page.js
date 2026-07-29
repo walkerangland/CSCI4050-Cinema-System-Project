0c517739f43e5f106b41cbb813da80aadcb0a9c0
cdggEX <cdgillon@yahoo.com>
2026-07-29 15:01:59 -0400

    Payment updates

--- app/payment/page.js
+++ app/payment/page.js
@@ -7,6 +7,7 @@ export default function Page({ params }) {
   const router = useRouter()
   const [cardData, setCardData] = useState('')
   const [loading, setLoading] = useState(true)
+  const [submitLoading, setSubmitLoading] = useState(false)
   const [isAddCard, setAddCard] = useState(false)
   const [cardForm, setCardForm] = useState({
     cardNumber: '', expirationMonth: '', expirationYear: '', cardholderName: ''
@@ -38,7 +39,8 @@ export default function Page({ params }) {
     setLoading(false)
   }, [])
 
-  const handleCardSubmit = async () => {
+  const handleCardSubmit = async (e) => {
+    e.preventDefault()
     const res = await fetch('/api/user/credit-cards', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json'},
@@ -50,33 +52,32 @@ export default function Page({ params }) {
       return
     }
     alert('Card updated!')
-    router.push(`/payment?${searchParams}`)
     window.location.reload()
   }
 
   const submitPayment = async (e) => {
     e.preventDefault()
-    setError('')
-    alert('submitted')
+    setSubmitLoading(true)
+    alert('Payment Submitted')
+    setSubmitLoading(false)
     router.push('/payment-confirmation')
   }
 
-const handleChange = (e) => {
-  setSelectCardForm({ ...selectCardForm, [e.target.name]: e.target.value })
-  console.log(selectCardForm)
-  setCardSelected(true)
-}
+  const handleChange = (e) => {
+    setSelectCardForm({ ...selectCardForm, [e.target.name]: e.target.value })
+    setCardSelected(true)
+  }
 
 
   if (loading) return <div style={{ color: '#fff', textAlign: 'center', marginTop: '4rem' }}>Loading...</div>
   return (
   <div style={{ maxWidth: '900px', margin: '0 auto', padding: '1rem', gap:'2rem', backgroundColor: '#0d0d0d', color: '#ffffff' }}>
-    <p>{selectCardForm.cardId}</p>
     <form onSubmit={submitPayment} id='selectCardForm'></form>
     <h1 style={{ fontSize: '2.0rem', fontWeight: 'bold', marginBottom: '1 rem', textAlign: 'center' }}>Secure Checkout</h1>
       <div style={{ fontFamily: 'sans-serif', fontSize: '1rem',maxWidth: '750px',  margin: '0 auto', marginBottom:'2rem', padding:'1rem', backgroundColor: '#232323', borderRadius: '12px', border: '1px solid #5a0000', color: '#ffffff' }}>
         <h1 style={{fontSize:'1.25rem'}}>Select a credit card to use:</h1>
         <div style ={{display:'flex', flexDirection:'column', gap:'1rem'}}>
+        <form onSubmit={handleCardSubmit} id='cardForm'></form>
         
         {cardData && cardData.length > 0 ? (
           cardData.map((card, index) => (
@@ -101,7 +102,7 @@ const handleChange = (e) => {
               )}
             {isAddCard && (
               <div>
-                <button onClick={() => handleCardSubmit()} style={{type:'button', padding: '0.4rem', backgroundColor: 'transparent', color: '#5972ff', outline: 'none', borderRadius: '8px', fontSize: '1rem', fontWeight: 'bold'}}>Submit</button>
+                <button type = 'submit' form = 'cardForm' style={{type:'button', padding: '0.4rem', backgroundColor: 'transparent', color: '#5972ff', outline: 'none', borderRadius: '8px', fontSize: '1rem', fontWeight: 'bold'}}>Submit</button>
                 <button onClick= {() => setAddCard(false)} style={{type:'button', padding: '0.4rem', backgroundColor: 'transparent', color: '#ff5959', outline: 'none', borderRadius: '8px', fontSize: '1rem', fontWeight: 'bold'}}>Cancel</button>
               </div>
             )}
@@ -109,18 +110,18 @@ const handleChange = (e) => {
               <div style={{ display: 'flex' }}>
                 <div style={{ padding: '0.5rem', maxWidth: '25%', wordWrap: 'break-word' }}>
                   <p style={{ fontWeight: 'bold' }}>Card Number:</p>
-                  <input type="text" name="cardNumber" value={cardForm.cardNumber} placeholder="Card Number" onChange={(e) => setCardForm({...cardForm, cardNumber: e.target.value})} />
+                  <input type="number" form="cardForm" name="cardNumber" value={cardForm.cardNumber} placeholder="Card Number" onChange={(e) => setCardForm({...cardForm, cardNumber: e.target.value})} />
                 </div>
                 <div style={{ padding: '0.5rem', maxWidth: '25%', wordWrap: 'break-word' }}>
                   <p style={{ fontWeight: 'bold' }}>Expiration Date:</p>
                   <div>
-                    <input type="number" max='12' name="expirationMonth" placeholder="MM" value={cardForm.expirationMonth} onChange={(e) => setCardForm({...cardForm, expirationMonth: e.target.value})} />
-                    <input type="number" name="expirationYear" placeholder="YYYY" value={cardForm.expirationYear} onChange={(e) => setCardForm({...cardForm, expirationYear: e.target.value})} />
+                    <input type="number" form="cardForm" max='12' name="expirationMonth" placeholder="MM" value={cardForm.expirationMonth} onChange={(e) => setCardForm({...cardForm, expirationMonth: e.target.value})} />
+                    <input type="number" form="cardForm" min= {new Date().getFullYear()} name="expirationYear" placeholder="YYYY" value={cardForm.expirationYear} onChange={(e) => setCardForm({...cardForm, expirationYear: e.target.value})} />
                   </div>
                 </div>
                 <div style={{ padding: '0.5rem', maxWidth: '25%', wordWrap: 'break-word' }}>
                   <p style={{ fontWeight: 'bold' }}>Name on card:</p>
-                  <input type="text" name="cardholderName" value={cardForm.cardholderName} placeholder="Cardholder Name" onChange={(e) => setCardForm({...cardForm, cardholderName: e.target.value})} />
+                  <input type="text" form="cardForm" name="cardholderName" value={cardForm.cardholderName} placeholder="Cardholder Name" onChange={(e) => setCardForm({...cardForm, cardholderName: e.target.value})} />
                 </div>
               </div>
         )}
@@ -158,7 +159,8 @@ const handleChange = (e) => {
       <div>
         {cardSelected && selectCardForm.cvv != '' && (
         <button
-          type = 'submit' 
+          type = 'submit'
+          disabled={submitLoading} 
           form = 'selectCardForm'
           style={{ width: '50%', padding: '0.75rem', backgroundColor: '#c0392b', color: '#ffffff', border: 'none', borderRadius: '8px', fontSize: '1rem', fontWeight: 'bold', justifyContent: 'center', alignItems: 'center', cursor: 'pointer', marginTop: '1rem' }}
           >Submit Payment</button>
